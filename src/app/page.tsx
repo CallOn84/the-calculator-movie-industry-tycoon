884fef5552cfeb39dd6e460ca7a4923a06523d23
Victor Franco <victor.oliveirafranco@gmail.com>
2025-03-04 17:46:23 -0300

    fix: message for next feature.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,12 +11,12 @@ export default function Home() {
     <main className="flex flex-col items-center justify-center p-2 w-full overflow-x-hidden md:m-16">
       <h1 className="text-3xl font-bold mb-6 text-center">{t.title}</h1>
       <AffinityCalculator />
-      {/* <div className="mt-8 p-4 text-center text-sm text-gray-600 dark:text-gray-300 w-full max-w-2xl mx-auto bg-white dark:bg-gray-800 rounded-xl shadow-md">
+      <div className="mt-8 p-4 text-center text-sm text-gray-600 dark:text-gray-300 w-full max-w-2xl mx-auto bg-white dark:bg-gray-800 rounded-xl shadow-md">
         <p>{t.nextFeatureMessage}</p>
-        <p className="font-bold mt-2 text-emerald-700 dark:text-emerald-500 py-1 px-2 rounded border border-emerald-300 bg-emerald-100 dark:border-emerald-300/10 dark:bg-emerald-400/10 md:w-4/5 mx-auto">
+        <p className="font-semibold mt-2 text-blue-700 dark:text-blue-500 py-1 px-2 rounded border border-blue-300 bg-blue-100 dark:border-blue-300/10 dark:bg-blue-400/10 md:w-4/5 mx-auto">
           {t.nextFeature}
         </p>
-      </div> */}
+      </div>
       <p className="text-xs mt-5 text-center">
         {t.messageUpdate1}{" "}
         <a
@@ -30,9 +30,9 @@ export default function Home() {
         </a>{" "}
         {t.messageUpdate2}
       </p>
-      
+
       {/* Botão de Doação do PayPal */}
-      
+
 
       <Script
         id="structured-data"
